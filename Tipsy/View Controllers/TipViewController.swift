5b7971369c855d9c419b8a1c545023ebd9ae7cc6
Gi Pyo Kim <gipyok@gmail.com>
2021-10-29 17:44:55 -0700

    Setup viewControllers and IBActions and IBOurlets

---
+++ Tipsy/View Controllers/TipViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TipViewController.swift
+//  Tipsy
+//
+//  Created by Gi Pyo Kim on 10/29/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+}
